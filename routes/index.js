971362a3c73e3e6d3c73ed3c3c271d642226d8c4
LMCMLJ <mjcalvey2@gmail.com>
2016-11-28 11:14:33 +0000

    Initial tech test MVP

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var object = {fire: "man"};
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/get', function(req, res) {
+  var key = Object.keys(req.query).toString();
+  res.send(object[req.query[key]]);
 });
 
-router.post('/', function(req, res) {
-  var key = req.param('key');
+router.post('/set', function(req, res) {
+  var key = Object.keys(req.query).toString();
+  object[key] = req.query[key];
+  console.log(object);
+  res.redirect('/get?key=' + Object.keys(object)[Object.keys(object).length - 1]);
 });
 
 module.exports = router;
